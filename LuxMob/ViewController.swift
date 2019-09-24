52c71a0e789a254f5a726d9b430a88552d29a2b4
Joe Mayer <lsb257@Mayers-Mac-mini.local>
2019-09-24 11:25:02 +0200

    Browser redirect for links

--- LuxMob/ViewController.swift
+++ LuxMob/ViewController.swift
@@ -13,11 +13,12 @@ import WebKit
 class ViewController: UIViewController, WKNavigationDelegate {
 
     var webView : WKWebView?
+    var websiteURL : String = "https://map.geoportail.lu/?localforage=ios&ipv6=true&applogin=yes"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // let url = URL(string: "http://wrk29.wrk.lsn.camptocamp.com:5000?localforage=ios&localhost")
-        let url = URL(string: "https://map.geoportail.lu/?localforage=ios&ipv6=true&applogin=yes") //new map link
+        let url = URL(string: websiteURL) //new map link
         // let url = URL(string: "https://offline-demo.geoportail.lu") // 100% functional, without native backend
         webView!.load(URLRequest(url: url!))
         webView!.allowsBackForwardNavigationGestures = true
@@ -33,5 +34,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
         controller.add(ScriptMessageHandler(webview: webView!), name: "ios")
         webView!.navigationDelegate = self
         view = webView
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.navigationType == .linkActivated  {
+            if let newURL = navigationAction.request.url,
+                let host = newURL.host , !host.hasPrefix(websiteURL) &&
+                UIApplication.shared.canOpenURL(newURL) {
+                print(newURL)
+                print("Redirected to browser. No need to open it locally")
+                decisionHandler(.cancel)
+                UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
+            } else {
+                print("Open it locally")
+                decisionHandler(.allow)
+            }
+        } else {
+            print("not a user click")
+            decisionHandler(.allow)
+        }
     }
 }
